3e4d820707d0c6388ae36100e02e79a96cd67ab0
shang <shang@qq.com>
2015-12-30 08:23:26 +0800

    提交代码

---
+++ demo2/demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo2
+//
+//  Created by CC on 15/12/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo2/demo2/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  demo2
+//
+//  Created by CC on 15/12/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"dayin");
+}
+
+
+@end
